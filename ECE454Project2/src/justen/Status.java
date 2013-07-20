7a81340c3cd3815e4c0c8ffd5010850085fec04b
Justen Pinto <justen.pinto@gmail.com>
2013-07-19 21:14:49 -0400

    status update

--- ECE454Project2/src/justen/Status.java
+++ ECE454Project2/src/justen/Status.java
@@ -9,9 +9,11 @@ public class Status implements Serializable {
 	private FileManager fileManager;
 	
 	public HashMap<String, ArrayList<Integer>> fileVersionMap; // fileName, 0001010: bit string for stable file verions
+	public HashMap<String, Lock> lockMap;
 	
 	public Status(FileManager fm) {
 		fileManager = fm;
 		fileVersionMap = fileManager.getAllFiles();
+		lockMap = fileManager.getLockMap();
 	}
 }
