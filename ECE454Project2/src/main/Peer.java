f0a3087b8c57209162278dc55eab3bd7dcb4c39d
Eric Cartmen <charlieouyang@gmail.com>
2013-07-20 11:58:48 -0400

    Let's Rev tonight

--- ECE454Project2/src/main/Peer.java
+++ ECE454Project2/src/main/Peer.java
@@ -16,28 +16,6 @@ public class Peer {
 		
 		UserInputThread userInputThread = new UserInputThread();
 		userInputThread.start();
-		
-		// 2) Invoke the File Client Thread		
-//		ClientBroadcastUp fileClientThread = new ClientBroadcastUp(PropertiesOfPeer.ipAddrPortNumMappingAll);
-//		fileClientThread.start();
-//		
-//		// Broadcast thread for status
-//		ClientBroadcastStatus statusBroadcastThread = new ClientBroadcastStatus(PropertiesOfPeer.ipAddrPortNumMappingAll);
-//		statusBroadcastThread.start();
-////		
-//		System.out.println("Running file client thread");
-		
-		//Debugging stuff
-		//PrintOutIpPortAliveMapThread debugThread = new PrintOutIpPortAliveMapThread();
-		//debugThread.start();
-		
-		//PrintOutStatusMappingsOfOtherPeers debugThread2 = new PrintOutStatusMappingsOfOtherPeers();
-		//debugThread2.start();
-		
-		//Shutdown the server!
-		//CloseThisConnectionThread closePeerThread = new CloseThisConnectionThread();
-		//closePeerThread.start();
-
 	}
 	
 	
