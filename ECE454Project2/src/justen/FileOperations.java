f8bdc30af8f1ddd12eba56fb52cc84d60a4d3ae9
jjpinto <jjpinto@WEEF-I12.NEXUS.UWATERLOO.CA>
2013-07-22 17:46:24 -0400

    shitttttt

--- ECE454Project2/src/justen/FileOperations.java
+++ ECE454Project2/src/justen/FileOperations.java
@@ -17,10 +17,10 @@ public class FileOperations
 	
 	public int open(String fileName, char operation) {
 		if (operation != 'r' || operation != 'w')
-			return -1;
+			return ReturnCode.INCORRECT_OPERATION;
 		
 		if (!fm.fileExists(fileName))
-			return -1;
+			return ReturnCode.FILE_DOES_NOT_EXIST;
 		
 		int v = fileName.lastIndexOf("_") + 2; 
 		String vNum = fileName.substring(v, fileName.lastIndexOf("."));
@@ -29,23 +29,23 @@ public class FileOperations
 		
 		if (!fm.containsFileLocally(fileName, versionNumber)) {
 			if (operation == 'w' && (otherDeviceHasReadLock(fileName) || !otherDeviceHasWriteLock(fileName)))
-				return -1; // prevents downloading file if other device has lock
+				return ReturnCode.OTHER_DEVICE_HAS_LOCK; // prevents downloading file if other device has lock
 			
 			if (operation == 'r' && otherDeviceHasWriteLock(fileName))
-				return -1;
+				return ReturnCode.OTHER_DEVICE_HAS_LOCK;
 			
 			
 			String device = getDeviceForFile(fileNameProper, versionNumber);
 			int port = Integer.parseInt(device.substring(device.indexOf(":") + 1, device.length()));
 			
 			if (!PropertiesOfPeer.CheckIfThisHostIsStillAlive(device.substring(0, device.indexOf(":")), port)) 
-				return -1;
+				return ReturnCode.HOST_NOT_ALIVE;
 			
 			ClientStateManager.GetFileFromOtherPeer(device, fileName);
 			if (openFile(fileName, operation))
 				PropertiesOfPeer.broadcastStatus();
 			else
-				return -1;
+				return ReturnCode.OTHER_DEVICE_HAS_LOCK;
 			
 		}
 		else {
@@ -53,45 +53,45 @@ public class FileOperations
 			if (openFile(fileName, operation))
 				PropertiesOfPeer.broadcastStatus();
 			else
-				return -1;
+				return ReturnCode.OTHER_DEVICE_HAS_LOCK;
 		}
 		return 0;
 	}
 	
 	public int close(String fileName) {
 		if (!fm.fileExists(fileName)) 
-			return -1;
+			return ReturnCode.FILE_DOES_NOT_EXIST;
 		if (!fm.isFileOpen(fileName))
-			return 1;
+			return ReturnCode.FILE_NOT_OPEN;
 		
 		return closeFile(fileName);
 	}
 	
 	public int create(String fileName) { //charlie.docx
 		if (fm.fileExists(fileName))
-			return -1;
+			return ReturnCode.FILE_ALREADY_EXISTS;
 		
 		String properName = fileName.substring(0, fileName.indexOf("."));
-		String extension = fileName.substring(fileName.indexOf(".") + 1);
-		File file = new File(properName + "_v1" + extension);
+		String extension = fileName.substring(fileName.indexOf("."));
+		File file = new File(PropertiesOfPeer.folderPeerName + "\\" + properName + "_v1" + extension);
 		try {
 			file.createNewFile();
 		} catch (IOException e) {
 			e.printStackTrace();
-			return -1;
+			return ReturnCode.COULD_NOT_CREATE_FILE;
 		}
 		
 		//TODO: Update the file manager
 		fm.addLocalFile(fileName);
 		PropertiesOfPeer.broadcastStatus();
-		return 0;
+		return ReturnCode.SUCCESS;
 	}
 	
 	public int delete(String fileName) {
 		if (!fm.fileExists(fileName))
-			return -1;
+			return ReturnCode.FILE_DOES_NOT_EXIST;
 		if (otherDeviceHasReadLock(fileName) || otherDeviceHasWriteLock(fileName))
-			return -1;
+			return ReturnCode.OTHER_DEVICE_HAS_LOCK;
 		
 		int v = fileName.lastIndexOf("_") + 2; 
 		String vNum = fileName.substring(v, fileName.lastIndexOf("."));
@@ -103,30 +103,30 @@ public class FileOperations
 		
 		PropertiesOfPeer.deleteFileBroadcast(fileName);
 		PropertiesOfPeer.broadcastStatus();
-		return 0;
+		return ReturnCode.SUCCESS;
 	}
 	
 	public int deleteAll(String fileName) {
 		if (fm.getVersionMap().containsKey(fileName))
-			return -1;
+			return ReturnCode.FILE_DOES_NOT_EXIST;
 		// check if any of the devices have a lock on any of the files
 		int numVersions = fm.getVersionMap().get(fileName).size();
 		String properName = fileName.substring(0, fileName.lastIndexOf("."));
 		String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
 		for (int i = 0; i < numVersions; i++) {
 			if (anyDeviceHasLockOnFile(properName, extension, i))
-				return -1;
+				return ReturnCode.OTHER_DEVICE_HAS_LOCK;
 		}
 		fm.removeLocalFile(fileName, true);
 		
 		PropertiesOfPeer.deleteFileAllVersionsBroadcast(fileName);
 		PropertiesOfPeer.broadcastStatus();
-		return 0;
+		return ReturnCode.SUCCESS;
 	}
 	
 	private int closeFile(String fileName) {
 		if (!fm.getLockMap().containsKey(fileName)) 
-			return -1;
+			return ReturnCode.FILE_DOES_NOT_EXIST;
 		else {
 			if (fm.getLockMap().get(fileName) instanceof ReaderLock) {
 				fm.getLockMap().put(fileName, null);
@@ -144,9 +144,9 @@ public class FileOperations
 				PropertiesOfPeer.broadcastStatus();
 			}
 			else // lock was null wtf?
-				return -1;
+				return ReturnCode.GO_FUCK_YOURSELF;
 		}
-		return 0;
+		return ReturnCode.SUCCESS;
 			
 	}
 	
--- ECE454Project2/src/justen/ReturnCode.java
+++ ECE454Project2/src/justen/ReturnCode.java
@@ -1,5 +1,19 @@
 package justen;
 
 public final class ReturnCode {
-
+	// SUCCESS
+	public static final int SUCCESS = 0;
+	
+	// ERRORS
+	public static final int INCORRECT_OPERATION = -1;
+	public static final int FILE_ALREADY_EXISTS = -2;
+	public static final int OTHER_DEVICE_HAS_LOCK = -3;
+	public static final int HOST_NOT_ALIVE = -4;
+	public static final int FILE_DOES_NOT_EXIST = -5;
+	
+	public static final int COULD_NOT_CREATE_FILE = -998;
+	public static final int GO_FUCK_YOURSELF = -999;
+	
+	// WARNINGS
+	public static final int FILE_NOT_OPEN = 1;
 }
